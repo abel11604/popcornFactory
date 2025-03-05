de9202d56be6734ea327c7a2196be6c2206dc465
abel11604 <abelcorona.quintero@gmail.com>
2025-03-05 16:38:08 -0700

    selección de asientos

--- app/src/main/java/mx/edu/itson/practica6/catalogo.kt
+++ app/src/main/java/mx/edu/itson/practica6/catalogo.kt
@@ -46,14 +46,14 @@ class catalogo : AppCompatActivity() {
                     "entre Tanjiro y la Cuarta Luna Creciente, Hatengu, además de cómo Nezuko logra caminar bajo el " +
                     "sol. Le seguirá el episodio 1 del Arco del Entrenamiento de los Pilares, donde veremos el inicio del " +
                     "entrenamiento de los Pilares para prepararse de cara a la próxima batalla contra Muzan Kibutsuji, " +
-                    "que se podrá ver por primera vez."))
+                    "que se podrá ver por primera vez.", arrayListOf<Cliente>()))
         peliculas.add(Pelicula(
             "Duna: Parte Dos",
             R.drawable.dune, R.drawable.dune2,
             "Duna: Parte Dos explorará el viaje mítico de Paul Atreides mientras se une con Chani y los " +
                     "Fremen mientras está en un camino de venganza contra los conspiradores que destruyeron a su " +
                     "familia. Enfrentando una elección entre el amor de su vida y el destino del universo conocido, se " +
-                    "esfuerza por evitar un futuro terrible que solo él pueda prever."
+                    "esfuerza por evitar un futuro terrible que solo él pueda prever.", arrayListOf<Cliente>()
         ))
 
         peliculas.add(Pelicula(
@@ -64,7 +64,7 @@ class catalogo : AppCompatActivity() {
                     "originales que han desarrollado un laboratorio de investigación ultrasecreto para llevar la cacería " +
                     "de fantasmas al siguiente nivel. Pero cuando el descubrimiento de un artefacto antiguo libera una " +
                     "fuerza maligna, los nuevos y viejos Ghostbusters unen sus fuerzas para proteger su hogar y salvar " +
-                    "al mundo de una segunda era de hielo."
+                    "al mundo de una segunda era de hielo.", arrayListOf<Cliente>()
         ))
 
         peliculas.add(Pelicula(
@@ -72,7 +72,7 @@ class catalogo : AppCompatActivity() {
             R.drawable.heroexencargo, R.drawable.heroeencargo,
             "Un ex agente de las fuerzas especiales acepta un trabajo para brindar seguridad a una periodista " +
                     "mientras entrevista a un dictador, pero estalla un golpe militar en medio de la entrevista y se ven " +
-                    "obligados a escapar a la jungla donde deben sobrevivir."
+                    "obligados a escapar a la jungla donde deben sobrevivir.", arrayListOf<Cliente>()
         ))
 
         peliculas.add(Pelicula(
@@ -80,7 +80,7 @@ class catalogo : AppCompatActivity() {
             R.drawable.vidaspasadas, R.drawable.vidaspasadas1,
             "Nora y Hae Sung, dos amigos de la infancia profundamente unidos, son separados después de que" +
                     "la familia de Nora emigrara de Corea del Sur. Veinte años después, se reúnen durante una fatídica " +
-                    "semana para enfrentarse al amor y al destino."
+                    "semana para enfrentarse al amor y al destino.", arrayListOf<Cliente>()
         ))
 
         peliculas.add(Pelicula(
@@ -91,7 +91,7 @@ class catalogo : AppCompatActivity() {
                     "thriller de suspenso protagonizado por Dakota Johnson como Cassandra Webb, una paramédico " +
                     "de Manhattan que puede tener habilidades clarividentes. Forzada a enfrentarse a revelaciones de " +
                     "su pasado, ella forja una relación con tres mujeres jóvenes destinadas a tener poderosos futuros… " +
-                    "si pueden sobrevivir a un presente mortal."
+                    "si pueden sobrevivir a un presente mortal.", arrayListOf<Cliente>()
         ))
 
     }
@@ -102,7 +102,7 @@ class catalogo : AppCompatActivity() {
             R.drawable.ant, R.drawable.ant,
             "La leyenda de Aang sigue al último sobreviviente de los Nómadas del Aire, quien deberá restaurar" +
                     "el equilibrio en el mundo entre las tres naciones restantes: la Tribu del Agua, el Reino Tierra y la " +
-                    "Nación del Fuego."
+                    "Nación del Fuego.", arrayListOf<Cliente>()
         ))
 
         series.add(Pelicula(
@@ -110,7 +110,7 @@ class catalogo : AppCompatActivity() {
             R.drawable.halo, R.drawable.halos,
             "Una evacuación mortal cambia la guerra del Jefe Maestro con el Covenant. En Reach, un nuevo " +
                     "régimen toma el mando en FLEETCOM. John siente una amenaza inminente cerca. Estreno de " +
-                    "temporada."
+                    "temporada.", arrayListOf<Cliente>()
         ))
 
         series.add(Pelicula(
@@ -119,28 +119,28 @@ class catalogo : AppCompatActivity() {
             "En un mundo en el que ciertos humanos llamados “cazadores” poseen habilidades mágicas, estos " +
                     "deben luchar contra monstruos para proteger a la raza humana de una aniquilación segura. Un " +
                     "cazador muy débil llamado Sung Jinwoo se encuentra en una lucha en la que solo puede tratar de " +
-                    "sobrevivir."
+                    "sobrevivir.", arrayListOf<Cliente>()
         ))
 
         series.add(Pelicula(
             "Mi adorable demonio",
             R.drawable.adorabledemonio, R.drawable.adorabledemonios,
             "Se centra en la vida de Jung Koo Won, un temerario demonio que pierde sus poderes tras conocer " +
-                    "a Do Do-Hee, una exitosa empresaria con la que deberá de colaborar para recuperarlos."
+                    "a Do Do-Hee, una exitosa empresaria con la que deberá de colaborar para recuperarlos.", arrayListOf<Cliente>()
         ))
 
         series.add(Pelicula(
             "El Monstruo de la Vieja Seúl",
             R.drawable.elmonstruo, R.drawable.elmonstruovieja,
             "Gyeongseong, 1945. En la oscura era colonial de Seúl, un empresario y una investigadora luchan " +
-                    "por sobrevivir y se enfrentan a un monstruo nacido de la avaricia humana."
+                    "por sobrevivir y se enfrentan a un monstruo nacido de la avaricia humana.", arrayListOf<Cliente>()
         ))
 
         series.add(Pelicula(
             "Witcher",
             R.drawable.thewitcher, R.drawable.thewitchers,
             "Geralt de Rivia, un cazador de monstruos mutante, viaja en pos de su destino por un mundo " +
-                    "turbulento en el que, a menudo, los humanos son peores que las bestias."
+                    "turbulento en el que, a menudo, los humanos son peores que las bestias.", arrayListOf<Cliente>()
         ))
     }
 }
@@ -181,6 +181,7 @@ class PeliculaAdapter: BaseAdapter {
             intento.putExtra("imagen", pelicula.image)
             intento.putExtra("header", pelicula.header)
             intento.putExtra("sinopsis", pelicula.sinopsis)
+            intento.putExtra("numberSeats",(20-pelicula.seats.size))
             context!!.startActivity(intento)
         }
         return vista
